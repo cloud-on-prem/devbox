50febc59cd93e5e2cb3f70c56fa4c433ca99d21c
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-09-12 12:48:14 +1000

    Adds bash aliases.

---
+++ stack/templates/bash_aliases.bash
@@ -0,0 +1,29 @@
+alias c='clear'
+alias refresh='source ~/.bashrc'
+alias restartnet="sudo nohup sh -c 'invoke-rc.d networking stop; date; echo sleeping; sleep 2; echo waking; date; invoke-rc.d networking start'"
+
+alias lss='ls -lAh'
+
+alias v='vim'
+
+alias vu='vagrant up'
+alias vh='vagrant halt'
+alias vs='vagrant ssh'
+alias vr='vagrant resume'
+alias vsp='vagrant suspend'
+
+alias tma='tmux a -t '
+alias tmn='tmux new -s '
+alias tmk='tmux kill-session -t '
+alias tmls='tmux ls'
+
+alias gfr="git fetch && git rebase"
+alias gco='git branch | cut -c 3- | selecta | xargs git checkout && clear'
+alias grb='git branch --merged master | grep -v "\* master" | xargs -n 0 git branch -d'
+alias gti='git'
+alias gset='git push --set-upstream origin `git symbolic-ref --short -q HEAD`'
+alias gpush='git push'
+alias groom='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+
+alias bx='bundle exec'
+alias fs='foreman start'
