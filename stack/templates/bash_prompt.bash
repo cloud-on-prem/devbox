fdb36c606136a8813cf390d24a07012a7759de24
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-09-16 11:55:01 +1000

    Better OS logo.

--- stack/templates/bash_prompt.bash
+++ stack/templates/bash_prompt.bash
@@ -1,8 +1,17 @@
+os_logo(){
+  if [[ $OSTYPE == darwin* ]]; then
+    echo ""
+  else
+    echo ""
+  fi
+}
+
 SCM_THEME_PROMPT_PREFIX="${cyan}(${blue}"
 SCM_THEME_PROMPT_SUFFIX="${cyan})"
 SCM_THEME_PROMPT_DIRTY="${red}"
 SCM_THEME_PROMPT_CLEAN="${green}●"
-LOGO_PREFIX="${orange}${reset_color} "
+LOGO="$(os_logo)"
+LOGO_PREFIX="${orange}${LOGO}${reset_color} "
 
 prompt() {
   PS1="${LOGO_PREFIX}$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} ${green}→${reset_color} "
