bee93c5919f5e839dc2aefdda9d5aba8d60604be
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-11-11 08:58:53 +1100

    Remove peco references.

--- stack/dev-box/templates/bash_functions.bash
+++ stack/dev-box/templates/bash_functions.bash
@@ -1,12 +1,3 @@
-h(){
-  eval $(history | peco | awk '{print $2}')
-}
-
-cdf() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | peco) && cd "$dir"
-}
-
 clean-docker(){
   docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 }
