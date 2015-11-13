d3d3ba8035d2a20f700e6aae4f518451d0199190
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-11-13 16:28:09 +1100

    Adds Bash My Aws

--- stack/dev-box/templates/bashrc.bash
+++ stack/dev-box/templates/bashrc.bash
@@ -33,3 +33,10 @@ source ~/.fzf.bash 2> /dev/null
 source $BASH_IT/bash_it.sh
 source ~/.context_bashrc 2> /dev/null
 source ~/.bash_prompt 2> /dev/null
+
+# Bash My Aws
+BASH_MY_AWS_DIR = ~/.bash-my-aws
+if [-d BASH_MY_AWS_DIR]; then
+  for f in ~/.bash-my-aws/lib/*-functions; do source $f; done
+  eval "$(~/.bash-my-aws/bin/generate_bash_completion)"
+fi
