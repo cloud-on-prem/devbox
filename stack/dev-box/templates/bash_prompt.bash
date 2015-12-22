56ce77ccfdee223ba3ffe214e6337eb236025213
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-12-22 16:38:53 +1100

    Delegates SCM stuff to Tmux.

--- stack/dev-box/templates/bash_prompt.bash
+++ stack/dev-box/templates/bash_prompt.bash
@@ -1,7 +1,7 @@
 SCM_THEME_PROMPT_PREFIX="${cyan}(${blue}"
 SCM_THEME_PROMPT_SUFFIX="${cyan})"
-SCM_THEME_PROMPT_DIRTY="${red}"
-SCM_THEME_PROMPT_CLEAN="${green}●"
+SCM_THEME_PROMPT_DIRTY="${red}"
+SCM_THEME_PROMPT_CLEAN="${green}"
 LOGO="{{ logo }}"
 LOGO_PREFIX="${orange}${LOGO}${reset_color} "
 LAST_ARROW="→"
@@ -17,7 +17,19 @@ exit_status() {
 
 prompt() {
   exit_status
-  PS1="${LOGO_PREFIX}$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} ${LAST_ARROW} "
+
+  if ! { [ -n "$TMUX" ]; } then
+    PS1="${LOGO_PREFIX}$(scm_prompt_info)${reset_color} ${cyan}\W${reset_color} ${LAST_ARROW} "
+  else
+    # TMUX has a special prompt
+    SCM_THEME_PROMPT_PREFIX=""
+    SCM_THEME_PROMPT_SUFFIX=""
+    SCM_THEME_PROMPT_DIRTY=""
+    SCM_THEME_PROMPT_CLEAN=""
+    tmux set-option -gq "@scm_info" "$(scm_prompt_info)"
+    tmux refresh-client -S
+    PS1="${LOGO_PREFIX}${reset_color} ${cyan}\W${reset_color} ${LAST_ARROW} "
+  fi
 }
 
 PROMPT_COMMAND=prompt
