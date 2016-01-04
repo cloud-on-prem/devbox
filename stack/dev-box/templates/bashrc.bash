5045792042e52074e65edcba296b6d61d52f63d8
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2016-01-04 20:25:17 +1100

    FZF now uses ag

--- stack/dev-box/templates/bashrc.bash
+++ stack/dev-box/templates/bashrc.bash
@@ -16,6 +16,7 @@ export FZF_DEFAULT_OPTS='
   --color=dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254
   --color info:254,prompt:37,spinner:108,pointer:235,marker:235
 '
+export FZF_DEFAULT_COMMAND='ag -g ""'
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 #Go
