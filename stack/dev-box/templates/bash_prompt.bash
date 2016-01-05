1cd23d6582717ba66da93f5ef9f4a0784cf48e07
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2016-01-05 12:58:04 +1100

    New art for git.

--- stack/dev-box/templates/bash_prompt.bash
+++ stack/dev-box/templates/bash_prompt.bash
@@ -1,7 +1,10 @@
+SCM_THEME_PROMPT_DIRTY_SYMBOL=""
+SCM_THEME_PROMPT_CLEAN_SYMBOL=""
+
 SCM_THEME_PROMPT_PREFIX="${cyan}(${blue}"
 SCM_THEME_PROMPT_SUFFIX="${cyan})"
-SCM_THEME_PROMPT_DIRTY=" ${red} "
-SCM_THEME_PROMPT_CLEAN=" ${green} "
+SCM_THEME_PROMPT_DIRTY=" ${red}${SCM_THEME_PROMPT_DIRTY_SYMBOL} "
+SCM_THEME_PROMPT_CLEAN=" ${green}${SCM_THEME_PROMPT_CLEAN_SYMBOL} "
 LOGO="{{ logo }}"
 LOGO_PREFIX="${orange}${LOGO}${reset_color} "
 LAST_ARROW="→"
@@ -30,8 +33,8 @@ update_tmux(){
   # TMUX has a special prompt
   SCM_THEME_PROMPT_PREFIX=""
   SCM_THEME_PROMPT_SUFFIX=""
-  SCM_THEME_PROMPT_DIRTY="  "
-  SCM_THEME_PROMPT_CLEAN="  "
+  SCM_THEME_PROMPT_DIRTY=" ${SCM_THEME_PROMPT_DIRTY_SYMBOL} "
+  SCM_THEME_PROMPT_CLEAN=" ${SCM_THEME_PROMPT_CLEAN_SYMBOL} "
   tmux set-option -gq "@scm_info" "$(scm_prompt_info)"
   tmux refresh-client -S
 }
