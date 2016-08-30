eaca0534c12791f4dbb51432a961dd15cde7b967
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2016-08-30 12:03:03 +1000

    Install emojify in usr/local/bin

--- stack/dev-box/scripts/install_emojify.sh
+++ stack/dev-box/scripts/install_emojify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 URL=https://raw.githubusercontent.com/mrowa44/emojify/master/emojify
-DEST=/usr/bin/emojify
+DEST=/usr/local/bin/emojify
 cd /tmp
 wget ${URL}
 sudo cp ./emojify ${DEST}
