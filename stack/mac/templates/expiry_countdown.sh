df6ace7912c7437d4f87ea86db480b8dcb05696d
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2016-11-08 15:45:28 +1100

    Fix expiry_countdown

--- stack/mac/templates/expiry_countdown.sh
+++ stack/mac/templates/expiry_countdown.sh
@@ -4,4 +4,6 @@ export PATH=/usr/local/bin:$PATH
 file=$1
 expiry=$(gdate -f ${file} +%s)
 now=$(gdate +%s)
-echo $((expiry-now)) | awk '{print int($1/60)":"int($1%60)}'
+if [ $expiry -ge $now ]; then
+  echo $((expiry-now)) | awk '{ printf " Token Expiry: %02d:%02d", int($1/60),int($1%60) }'
+fi
