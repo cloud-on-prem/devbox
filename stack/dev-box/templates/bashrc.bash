e68ccc667694a1fdaecd58ac4813044431aa1a1a
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-04-29 10:28:20 +1000

    Introduce Ripgrep

--- stack/dev-box/templates/bashrc.bash
+++ stack/dev-box/templates/bashrc.bash
@@ -24,7 +24,7 @@ export HOSTMACHINE_USER="{{ hostmachine_user }}"
 # FZF
 export FZF_TMUX=0
 export FZF_DEFAULT_OPTS='--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --color info:144,prompt:161,spinner:135,pointer:135,marker:118'
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow'
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 . ~/.fzf.bash 2> /dev/null
 
