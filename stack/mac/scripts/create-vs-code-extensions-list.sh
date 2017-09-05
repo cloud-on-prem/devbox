1ca6e02d4d13503027a3e04e59d75f7c0740e347
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2017-09-05 12:24:34 +1000

    Add new plugins to VS Code

--- stack/mac/scripts/create-vs-code-extensions-list.sh
+++ stack/mac/scripts/create-vs-code-extensions-list.sh
@@ -2,4 +2,4 @@
 set -euf -o pipefail
 
 cd "$(dirname "$0")"
-code --list-extensions > "../templates/vs-code-extensions.txt"
+code --list-extensions | sort -f > "../templates/vs-code-extensions.txt"
