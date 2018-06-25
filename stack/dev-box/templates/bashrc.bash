519949c8194ea1777886177a261a87322a6cc707
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2018-06-25 22:55:28 +1000

    WIP

--- stack/dev-box/templates/bashrc.bash
+++ stack/dev-box/templates/bashrc.bash
@@ -16,10 +16,6 @@ export LC_ALL="en_AU.UTF-8"
 export LC_CTYPE="en_AU.UTF-8"
 export PATH="/usr/local/sbin:$PATH"
 export ANSIBLE_NOCOWS=1
-{% if guest_machine %}
-export HOSTMACHINE_IP="{{ hostmachine_ip_raw.stdout }}"
-export HOSTMACHINE_USER="{{ hostmachine_user }}"
-{% endif %}
 
 # FZF
 export FZF_TMUX=0
@@ -27,12 +23,6 @@ export FZF_DEFAULT_OPTS='--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81 --co
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow'
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 . ~/.fzf.bash 2> /dev/null
-
-#Go
-export PATH="/usr/local/go/bin:$PATH"
-export GOPATH="{{ workspace_dir }}/go"
-export PATH=$PATH:$GOPATH/bin
-export GO15VENDOREXPERIMENT=1
 
 #Haskell
 export PATH="$HOME/.stack/programs/x86_64-linux/ghc-7.10.3/bin/:$PATH" # Add Haskell path
@@ -50,27 +40,11 @@ export PATH="$HOME/.config/yarn/global/node_modules/.bin/:$PATH" # Add Yarn Glob
 
 # Load stuff without moaning
 
-{% if ansible_distribution == 'MacOSX' %}
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-{% endif %}
-{% if ansible_distribution == 'Fedora' %}
-[ -f /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
-{% endif %}
-
 . ~/.bash_functions 2> /dev/null
 . ~/.bash_aliases 2> /dev/null
 . ~/.bin/tmuxinator.bash 2> /dev/null
 . "$BASH_IT/bash_it.sh"
 . ~/.context_bashrc 2> /dev/null
 . ~/.bash_prompt 2> /dev/null
-
-# Bash My Aws
-BASH_MY_AWS_DIR=~/.bash-my-aws
-if [ -d BASH_MY_AWS_DIR ]; then
-  for f in $BASH_MY_AWS_DIR/lib/*-functions; do . $f; done
-  . $BASH_MY_AWS_DIR/bash_completion.sh
-fi
 
 unalias tree 2> /dev/null # use real tree
