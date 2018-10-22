377487c0567539295fd6020d0f2a2a50833e46ba
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2018-10-22 15:11:55 +1100

    Remove unused bash aliases

--- stack/dev-box/templates/bash_aliases.bash
+++ stack/dev-box/templates/bash_aliases.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 alias c='clear'
 alias refresh='source ~/.bashrc'
 alias restartnet="sudo nohup sh -c 'invoke-rc.d networking stop; date; echo sleeping; sleep 2; echo waking; date; invoke-rc.d networking start'"
@@ -6,22 +8,6 @@ alias push=pushover
 alias lss='ls -lAh'
 
 alias v='nvim'
-
-
-{% if guest_machine %}
-alias vu='echo "you are in a Vagrant box ;)"'
-alias vh='echo "you are in a Vagrant box ;)"'
-alias vs='echo "you are in a Vagrant box ;)"'
-alias vr='echo "you are in a Vagrant box ;)"'
-alias vsp='echo "you are in a Vagrant box ;)"'
-{% else %}
-alias vu='vagrant up'
-alias vh='vagrant halt'
-alias vs='vagrant ssh'
-alias vr='vagrant resume'
-alias vsp='vagrant suspend'
-{% endif %}
-
 
 alias tma='tmux a -t '
 alias tmk='tmux kill-session -t '
@@ -33,9 +19,6 @@ alias gti='git'
 alias gset='git push --set-upstream origin `git symbolic-ref --short -q HEAD`'
 alias gpush='git push'
 alias groom='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-
-alias bx='bundle exec'
-alias fs='foreman start'
 
 alias wk='cd {{ workspace_dir }}'
 alias play='cd {{ workspace_dir }}/play'
